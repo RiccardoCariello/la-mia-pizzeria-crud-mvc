a43ac0769fda389a1720a7458e7fc7396837ae6e
MillionDollarBob <riccardo.c1995@outlook.com>
2023-05-17 14:59:49 +0200

    costruttore

--- La Mia Pizzeria/La Mia Pizzeria/Models/PizzaModel.cs
+++ La Mia Pizzeria/La Mia Pizzeria/Models/PizzaModel.cs
@@ -1,6 +1,31 @@
-﻿namespace La_Mia_Pizzeria.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace La_Mia_Pizzeria.Models
 {
-    public class Pizza
+    public class PizzaModel
     {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(40)]
+        public string Name { get; set; }
+        
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+       
+        [MaxLength(300)]
+        public string ImgSource { get; set; }
+        
+        float Price { get; set; }
+
+        public PizzaModel(string name, string description , string imgSource, float price)
+        {
+            Name = name;
+            Description = description;
+            ImgSource = imgSource;
+            Price = price;
+        }
+
+
     }
 }
