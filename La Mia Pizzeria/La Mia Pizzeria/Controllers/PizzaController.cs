84d01805e3bd791c4a34ceddcdabfdbac26a76c9
MillionDollarBob <riccardo.c1995@outlook.com>
2023-05-17 17:47:06 +0200

    Fine?

--- La Mia Pizzeria/La Mia Pizzeria/Controllers/PizzaController.cs
+++ La Mia Pizzeria/La Mia Pizzeria/Controllers/PizzaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using La_Mia_Pizzeria.Models;
+using La_Mia_Pizzeria.Database;
 
 namespace La_Mia_Pizzeria.Controllers
 {
@@ -6,10 +8,35 @@ namespace La_Mia_Pizzeria.Controllers
     {
         public IActionResult Index()
         {
-            List<PizzaModel> 
-            
-            
-            return View();
+            using (PizzaContext db = new PizzaContext())
+            {
+                List<PizzaModel> pizze = db.Pizze.ToList();
+
+                return View(pizze);
+            }
+
+
+               
+        }
+
+        public IActionResult PizzaDetails(int Id)
+        {
+
+            using (PizzaContext db = new PizzaContext())
+            {
+
+                PizzaModel? pizza = db.Pizze.Where( pizze => pizze.Id == Id).FirstOrDefault();
+
+                if (pizza != null)
+                {
+
+
+                    return View(pizza);
+                }else
+                {
+                    return NotFound($"Nessuna pizza trovata con l'Id {Id}");
+                }
+            }
         }
     }
 }
